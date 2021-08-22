cc9c9b936e6a26b325732c654bdf6ca635a16f9a
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-22 22:44:08 +0100

    outline

---
+++ index.js
@@ -0,0 +1,19 @@
+// First create all of the classes with there methods and properties (anything 
+//that extends needs to use super)
+
+// Write unit tests for each method
+
+// when the user runs the index.js they are presented with a menu
+// this menu with give them 4 options [add engineer, add intern, add manager, generate html]
+// if one of the add options have been chosed lets say add engineer we will
+// prompt them with with all of the questions that will allow use to create an engineer with a class
+// once we have created the engineer we will push it into an array called team then rebuild the menu
+// once the user is ready to build the html they can select generate html which will use the team
+// array to create the list of employees
+
+// functions that should be create as a results of this
+
+// buildMenu() this function will use inquire to build the menu [add engineer, add intern, add manager, generate html]
+// when an option is selected from this menu we will run a function for it
+
+// addEngineer() this function will be from the result of buildMenu()
