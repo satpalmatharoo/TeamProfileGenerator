d819a4a9935f35688220046c8d4b0a56b51b6e4b
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-08-29 23:00:27 +0100

    correct func error

--- index.js
+++ index.js
@@ -1,91 +1,84 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require ("util");
 
+const Manager = require("./lib/Manager");
 
-// writeFile function
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const testing = () => {
-
-return inquirer.prompt([
+const promptBuildMenu = () => {
+  return inquirer.prompt([
     {
-    type: "input",
-    message: "What is the title of the project?",
-    name: "title",
-  },
+    type: "list",
+    message: "Please select one option from below",
+    choices: ["Add Manager", "Add Enginner", "Add Intern", "Generate HTML"]
+    },
+  ])
+}
+
+
+const promptManager = () => {
+return inquirer.promptManager([
   {
     type: "input",
-    message: "Give a detailed overview and description of the project",
-    name:  "Description",
+    message: "What is your name?",
+    name: "name",
   },
+
   {
     type: "input",
-    name: "Installation",
-    message: "Provide any installation instructions",
+    message: "Please enter employee id",
+    name: "id",
   },
-  {
   
+  {
     type: "input",
-    name: "Usage",
-    message: "Please explain usage",
+    message: "What is your role?",
+    name:  "role",
   },
+  
+  {
+    type: "input",
+    name: "What is your email",
+    message: "Email",
+  },
+
   {
     type: "list",
-    name: "License",
-    message: "Add your license or badge link if needed",
-    choices: ["GNU GPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "The Unlicense", "N/A"]
+    name: "Add Employee",
+    message: "Would you like to add another employee?",
+    choices: ["Engineer", "Intern", "No"],
   },
+
+  .then((response) => {
+    const manager =new manager(response.name, response.role, response.email))
+  };
+
+promptEngineer = () => {
+    return inquirer.promptEngineer([
+
   {
     type: "input",
     name: "Github",
     message: "Add your Github user name",
   },
-  {
-    type: "input",
-    name: "Email",
-    message: "Please add your email address",
-  },
-  {
-    type: "input",
-    name: "Contributing",
-    message: "Would you like to add contributors to your project?",
-  },
-  {
-    type: "input",
-    name: "Tests",
-    message: "Do you have any project tests? If so please provide",
-  },
+
+promptIntern = () => {
+  return inquirer.promptIntern([
+    {
+      type: "input",
+      name: "School",
+      message: "Which school",
+    };
+  ])
+
+}
 
 ])
 }
 
-const generateMarkdown = (answers) =>
-`## Table of contents
-* Title 
-* Description 
-* Installation
-* Usage
-* License
-* What is your Github Username
-* What is your Email address
-* Contributing
-* Tests
-   
- #${answers.title}
   
-   #${answers.Description}
-   #${answers.Installation}
-   #${answers.Usage}
-   #${answers.License}
-   #${answers.Github}
-   #${answers.Email}
-   #${answers.Contributing}
-   #${answers.Tests}
-  `;
-
 const init = () => {
-  testing()
+  promptBuildMenu()
     .then((data) => writeFileAsync('index.md', generateMarkdown(data)))
     .then(() => console.log('Successfully wrote to index.md'))
     .catch((err) => console.error(err));
